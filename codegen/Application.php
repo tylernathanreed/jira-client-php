1811de731f7b5d7d022d84dec93466635845158c
Tyler Reed <tylernathanreed@gmail.com>
2025-02-26 08:47:56 -0600

    - Dropped Zero Framework

--- codegen/Application.php
+++ codegen/Application.php
@@ -2,10 +2,52 @@
 
 namespace Jira\CodeGen;
 
-use LaravelZero\Framework\Application as BaseApplication;
+use Illuminate\Contracts\Console\Kernel as KernelContract;
+use Illuminate\Contracts\Debug\ExceptionHandler as HandlerContract;
+use Illuminate\Events\EventServiceProvider;
+use Illuminate\Foundation\Application as BaseApplication;
+use Illuminate\Foundation\Configuration\ApplicationBuilder;
+use Illuminate\Foundation\Exceptions\Handler;
+use Illuminate\Foundation\PackageManifest;
 
 class Application extends BaseApplication
 {
+    public static function configure(?string $basePath = null): ApplicationBuilder
+    {
+        $basePath = match (true) {
+            is_string($basePath) => $basePath,
+            default => static::inferBasePath(),
+        };
+
+        $builder = (new ApplicationBuilder(new static($basePath))); // @phpstan-ignore-line
+
+        $builder->create()->singleton(KernelContract::class, Kernel::class);
+
+        $builder->create()->singleton(HandlerContract::class, Handler::class);
+
+        return $builder
+            ->withEvents()
+            ->withCommands()
+            ->withProviders();
+    }
+
+    /** @inheritdoc */
+    protected function registerBaseBindings(): void
+    {
+        parent::registerBaseBindings();
+
+        /*
+         * Ignores auto-discovery.
+         */
+        $this->make(PackageManifest::class)->manifest = [];
+    }
+
+    /** @inheritdoc */
+    protected function registerBaseServiceProviders(): void
+    {
+        $this->register(new EventServiceProvider($this));
+    }
+
     /** @inheritdoc */
     public function version()
     {
@@ -16,5 +58,23 @@ class Application extends BaseApplication
     public function registerConfiguredProviders(): void
     {
         // Do nothing
+    }
+
+    /** @inheritdoc */
+    public function runningInConsole(): bool
+    {
+        return true;
+    }
+
+    /** @inheritdoc */
+    public function isDownForMaintenance(): bool
+    {
+        return false;
+    }
+
+    /** @inheritdoc */
+    public function configurationIsCached(): bool
+    {
+        return false;
     }
 }
--- codegen/Kernel.php
+++ codegen/Kernel.php
@@ -4,26 +4,55 @@ namespace Jira\CodeGen;
 
 use Illuminate\Config\Repository;
 use Illuminate\Console\Application as Artisan;
+use Illuminate\Filesystem\FilesystemServiceProvider;
+use Illuminate\Foundation\Console\Kernel as BaseKernel;
+use Illuminate\Support\Facades\Facade;
 use Jira\CodeGen\Commands;
-use LaravelZero\Framework\Kernel as BaseKernel;
 use ReflectionClass;
 use Symfony\Component\Console\Command\ListCommand;
+use Symfony\Component\Process\Process;
+use Illuminate\Foundation\Bootstrap;
 
 class Kernel extends BaseKernel
 {
     /** @inheritdoc */
     protected $bootstrappers = [
-        \LaravelZero\Framework\Bootstrap\CoreBindings::class,
-        // \LaravelZero\Framework\Bootstrap\LoadEnvironmentVariables::class,
-        // \LaravelZero\Framework\Bootstrap\LoadConfiguration::class,
-        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
-        \LaravelZero\Framework\Bootstrap\RegisterFacades::class,
-        \LaravelZero\Framework\Bootstrap\RegisterProviders::class,
-        \Illuminate\Foundation\Bootstrap\BootProviders::class,
+        Bootstrap\HandleExceptions::class,
+        Bootstrap\RegisterProviders::class,
+        Bootstrap\BootProviders::class,
     ];
 
     /** @inheritdoc */
     public function bootstrap(): void
+    {
+        $this->registerCoreBindings();
+        $this->loadConfiguration();
+        $this->registerFacadeRoot();
+        $this->registerCoreProviders();        
+
+        parent::bootstrap();
+
+        $this->getArtisan()->setName('Jira Client');
+    }
+
+    protected function registerCoreBindings(): void
+    {
+        $this->app->bind(
+            'git.version',
+            function (Application $app) {
+                $process = Process::fromShellCommandline(
+                    'git describe --tags --abbrev=0',
+                    $app->basePath()
+                );
+
+                $process->run();
+
+                return trim($process->getOutput()) ?: 'unreleased';
+            }
+        );
+    }
+
+    protected function loadConfiguration(): void
     {
         $this->app['env'] = 'production';
         $this->app->instance('config', new Repository([
@@ -31,8 +60,18 @@ class Kernel extends BaseKernel
                 'providers' => [],
             ]
         ]));
+    }
 
-        parent::bootstrap();
+    protected function registerFacadeRoot(): void
+    {
+        Facade::clearResolvedInstances();
+
+        Facade::setFacadeApplication($this->app);
+    }
+
+    protected function registerCoreProviders(): void
+    {
+        $this->app->register(FilesystemServiceProvider::class);
     }
 
     /** @inheritdoc */
@@ -54,11 +93,6 @@ class Kernel extends BaseKernel
             }
         );
 
-        /*
-         * Registers a bootstrap callback on the artisan console application
-         * in order to call the schedule method on each Laravel Zero
-         * command class.
-         */
         Artisan::starting(
             function ($artisan) {
                 $artisan->resolveCommands([
@@ -69,6 +103,18 @@ class Kernel extends BaseKernel
                 $artisan->setContainerCommandLoader();
             }
         );
-    }
 
+        Artisan::starting(
+            function ($artisan) {
+                foreach ($artisan->all() as $command) {
+                    if ($command instanceof ListCommand) {
+                        $command->setHidden(true);
+                    }
+
+                    $artisan->setContainerCommandLoader();
+                    $command->setApplication($artisan);
+                }
+            }
+        );
+    }
 }
